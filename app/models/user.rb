82eeb706aba9c1f28f0d4c111e9f369742bf06be
chungcha <charlieschung@gmail.com>
2019-11-09 11:46:03 -0500

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+    has_many :favorites
+
+    def index
+        users = User.all
+
+        render JSON 
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.new("username": "heej", "password": "flatiron1")
+user2 = User.new("username": "charlie", "password": "password")
+
+favorite1 = Favorite.new("user_id": user1.id, "location_id": "seoul1")
+favorite2 = Favorite.new("user_id": user2.id, "location_id": "detroint1")
