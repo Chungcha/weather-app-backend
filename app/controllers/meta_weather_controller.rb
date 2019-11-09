4a959ea55d64fe8c6be616b4a44bed989f144962
chungcha <charlieschung@gmail.com>
2019-11-09 16:05:56 -0500

    search function

--- app/controllers/meta_weather_controller.rb
+++ app/controllers/meta_weather_controller.rb
@@ -1,7 +1,17 @@
 class MetaWeatherController < ApplicationController
+
     def test
         response = RestClient.get("https://www.metaweather.com/api/location/search/?query=London")
         jsonResponse = JSON.parse(response.body)
         render json: jsonResponse
     end 
+
+    def search
+        searchCity = params["search"]
+        response = RestClient.get("https://www.metaweather.com/api/location/search/?query=#{searchCity}")
+        jsonResponse = JSON.parse(response.body)
+
+        render json: jsonResponse
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :favorites
   get "/test", to: "meta_weather#test"
+  post "/search", to: "meta_weather#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
