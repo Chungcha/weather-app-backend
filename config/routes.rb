06683f71c99fa8d2ffcc5d17d2935e62fce4364f
chungcha <charlieschung@gmail.com>
2019-11-09 22:47:16 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :index, :show]
+  resources :users
   resources :favorites, only: [:create, :destroy]
   get "/test", to: "meta_weather#test"
   post "/search", to: "meta_weather#search"
   post "/location", to: "meta_weather#location"
   post "/geolocation", to: "meta_weather#geolocation"
-  post "/login", to: "user#login"
+  post "users/login", to: "users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
