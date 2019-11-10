4d3b821c1d842397bfc0408a6357ed476059a411
chungcha <charlieschung@gmail.com>
2019-11-10 16:28:43 -0500

    accept changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,27 @@ class UsersController < ApplicationController
         render html: users
     end
 
+    # def login
+    #     user = User.find_by(username: params["username"])
+
+    #     # render json: user.favorites, except: [:updated_at, :created_at]
+    #     render json: user
+    # end
+
     def login
         user = User.find_by(username: params["username"])
 
-        render json: user.favorites, except: [:updated_at, :created_at]
+        render json: user.to_json(serialize)
+    end
+
+    private 
+
+    def serialize
+        { :include => { :favorites => {
+                        :except => [:created_at, :updated_at]
+                        }},
+        :only => [:username, :id]
+        }
     end
 
 end
