d17952686ae82068dc2c78781ff0ba27e6506d3a
chungcha <charlieschung@gmail.com>
2019-11-09 20:34:02 -0500

    finished some gelocation

--- app/controllers/meta_weather_controller.rb
+++ app/controllers/meta_weather_controller.rb
@@ -20,8 +20,15 @@ class MetaWeatherController < ApplicationController
         render json: response 
     end 
 
-    def geoLocation
-        location = params[""]
+    def geolocation
+        location = params["coordinates"]
+        response = RestClient.get("https://www.metaweather.com/api/location/search/?lattlong=#{location}")
+        jsonResponse = JSON.parse(response.body)
+        locationId = jsonResponse.first["woeid"]
+
+        response = RestClient.get("https://www.metaweather.com/api/location/#{locationId}/")
+
+        render json: response 
     end
 
 end
