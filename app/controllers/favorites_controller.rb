6cf63c26b6eef6c168776420faa2378c461d0f19
chungcha <charlieschung@gmail.com>
2019-11-11 22:59:59 -0500

    favorites button done

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,14 @@
 class FavoritesController < ApplicationController
 
     def create
-        favorite = Favorite.create(location_id: params[id], user_id: params[id])
+        favorite = Favorite.create(location_id: params[:woeId], user_id: params[:userId])
+
+        render json: favorite
     end
 
-    # def destroy
-    # end
+    def destroy
+        favorite = Favorite.find(params[:id])
+        favorite.destroy
+    end
 
 end
